e94d4b29d4252ea9938d17c1a8fd62c7568c20be
Estudiante <estuam@UAM.LOCAL>
2017-02-25 09:38:17 -0500

    add views

--- app/Http/Controllers/SitioController.php
+++ app/Http/Controllers/SitioController.php
@@ -9,10 +9,11 @@ class SitioController extends Controller
     //
 
     public function index(){
-        echo "Hola Mundo";
+       return view('sitio/index');
     }
 
     public function saludar(Request $request,$nombre){
-        echo "Hello ". $nombre;
+        return view('sitio/saludar', ['elNombre' => $nombre]);
+
     }
 }
